7fd6e322f9d5ccdc471ea854f9b37cdefe638537
Brian Morillo <morillob@wit.edu>
2023-11-13 01:25:05 -0500

    Fixed window not responding when closed

--- states/screen.py
+++ states/screen.py
@@ -66,6 +66,7 @@ class Screen(State):
         """checks events for the screen"""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                self.game_instance().client.client_socket.close()
                 sys.exit()
                 
             self._check_events(event)
